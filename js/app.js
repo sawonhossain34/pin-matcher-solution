fe8697ad7c8c0a70a532b1b7376cf41c55b2f3af
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-15 12:05:46 +0600

    complete add calculate section

---
+++ js/app.js
@@ -0,0 +1,44 @@
+function getPin(){
+    const pin = generatePin();
+    const pinString = pin + '';
+    if( pinString.length === 4){
+        return pin;
+    }else{
+        // console.log('pin not 3 digite ', pin);
+        return getPin();
+    }
+}
+
+function generatePin(){
+    const random = Math.round(Math.random()*10000);
+    return random;
+}
+
+document.getElementById('generate-pin').addEventListener('click',function(){
+    
+    const pin = getPin();
+    // display pin
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+})
+
+document.getElementById('calculator').addEventListener('click',function(event){
+    const number = event.target.innerText;
+    const typedNumberField =document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+
+    if(isNaN(number)){
+        if(number === 'C'){
+            typedNumberField.value = '';
+        }else if(number === '<'){
+            const digite = previousTypedNumber.split('');
+           digite.pop();
+           const remainingDigites = digite.join('');
+           typedNumberField.value = remainingDigites;
+        }
+    }
+    else{
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+})
